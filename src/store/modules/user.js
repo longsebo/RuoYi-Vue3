e0806aa1383ed7c3f6f40537da5f3ef31fc1789c
longsebo <7432731@qq.com>
2023-11-24 15:43:03 +0800

    参考vue2修复了好多链接错误

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import { login, logout, getInfo } from '@/api/login'
-import { getToken, setToken, removeToken } from '@/utils/auth'
+import { getToken, setToken, removeToken,setExpiresIn,getExpiresIn } from '@/utils/auth'
 import defAva from '@/assets/images/profile.jpg'
 
 const useUserStore = defineStore(
@@ -22,8 +22,9 @@ const useUserStore = defineStore(
         const uuid = userInfo.uuid
         return new Promise((resolve, reject) => {
           login(username, password, code, uuid).then(res => {
-            setToken(res.token)
-            this.token = res.token
+            setToken(res.data.access_token)
+            this.token = res.data.access_token
+            setExpiresIn(res.data.expires_in)
             resolve()
           }).catch(error => {
             reject(error)
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
-
+const ExpiresInKey = 'Admin-Expires-In'
 export function getToken() {
   return Cookies.get(TokenKey)
 }
@@ -12,4 +12,16 @@ export function setToken(token) {
 
 export function removeToken() {
   return Cookies.remove(TokenKey)
+}
+
+export function getExpiresIn() {
+  return Cookies.get(ExpiresInKey) || -1
+}
+
+export function setExpiresIn(time) {
+  return Cookies.set(ExpiresInKey, time)
+}
+
+export function removeExpiresIn() {
+  return Cookies.remove(ExpiresInKey)
 }
