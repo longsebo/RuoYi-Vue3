6f63db52a5f5299e1cbdef705b1d2e985148d7d3
longsebo <lxb741126>
2024-05-08 22:15:17 +0800

    开始排序组件

--- src/views/business/designsql/sortTabPane.vue
+++ src/views/business/designsql/sortTabPane.vue
@@ -1,5 +1,20 @@
 <template>
-
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <el-container>
+          <el-aside width="400px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-aside>
+      <el-main >
+        <el-container>
+          <el-aside width="400px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts">
