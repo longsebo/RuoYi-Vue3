e139f4a68841232bb3bf7cfc8923c44b46a4a0ad
longsebo <lxb741126>
2024-05-13 21:59:13 +0800

    改变拖动分隔条改变main,footer 高度

--- src/views/business/designsql/index.vue
+++ src/views/business/designsql/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <el-container style="height: 100vh;">
+  <el-container class="container">
     <el-header>
       <el-row :gutter="10" class="mb8">
         <el-col :span="1.5"><el-button link type="primary" icon="Plus" @click="handleAdd()" v-hasPermi="['business:interface:adddesignsql']">新增</el-button></el-col>
         <el-col :span="1.5"><el-button link type="primary" icon="Delete" @click="handleDelete()" v-hasPermi="['business:interface:removedesignsql']">删除</el-button></el-col>
       </el-row>
     </el-header>
-    <el-main>
+    <el-main :style="{ height: topHeight + 'px' }">
       <TableDraggingComponent :modelDefs="designModel.tablesModel" @updateTableDefine="updateTableDefine"  />
     </el-main>
-    <el-footer>
-      <div class="resizable" v-resize="onResize">
+    <div class="splitter" @mousedown="startResize" @touchstart="startResize"></div>
+    <el-footer class="bottom" :style="{ height: bottomHeight + 'px' }">
         <el-tabs v-model="activeName" @tab-click="handleElTabClick">
           <el-tab-pane label="列" name="columnTab">
             <ColumnTabPane v-model:selectColumnTabModel="designModel.value.selectColumnTabModel" :distinct="designModel.value.distinct" @updateDistinct="updateDistinct"/>
@@ -25,7 +25,6 @@
             <SqlPreviewTabPane/>
           </el-tab-pane>
         </el-tabs>
-      </div>
     </el-footer>
     <!--添加表 -->
     <el-dialog title="选择表" v-model="openSelectTab" width="500px" append-to-body>
@@ -59,238 +58,248 @@ import SearchConditionTabPane from "@/views/business/designsql/searchConditionTa
 import SortTabPane from "@/views/business/designsql/sortTabPane.vue"
 import ColumnTabPane from "@/views/business/designsql/sqlPreviewTabPane.vue"
 import SqlPreviewTabPane from "./sqlPreviewTabPane.vue";
+const topHeight = ref(100);
+const bottomHeight = ref(300);
+let startY = 0;
+let startTopHeight = 0;
+
 interface Emits {
   (e: 'updateDesignModel', updateDesignModel: object): void
 }
+const startResize = (event) => {
+  event.preventDefault();
+  startY = event.clientY;
+  startTopHeight = topHeight.value;
+  document.addEventListener('mousemove', doResize);
+  document.addEventListener('touchmove', doResize);
+  document.addEventListener('mouseup', stopResize);
+  document.addEventListener('touchend', stopResize);
+};
 
-    const emit = defineEmits<Emits>()
-    const  form=ref({});
-    const  tableFormRules=ref({
-        tableEnName: [
-          { required: true, message: "请选择表", trigger: "blur" }
-        ],
-      })
-    const  treeModelDef=ref([])
-    const  openSelectTab=ref(false)
-    const designModel =ref({
-      selectColumnTabModel:{},//列表模型
-      distinct:Boolean,//是否排重
-      conditionTreeModel:{},//条件列树模型
-      groupConditionTreeModel:{},//分组列树模型
-      tablesModel:[],//表模型列表
-      sortColumnModel:[],//排序列模型
-      tableJoinModels:[],//表关系模型
-    })
-   const activeName=ref('columnTab')
+const doResize = (event) => {
+  event.preventDefault();
+  const diff = event.clientY - startY;
+  topHeight.value = startTopHeight + diff;
+  bottomHeight.value = 500 - topHeight.value; // 假设容器高度是500px
+};
 
-    const props= defineProps({
-      designModel: {
-        type:Object,
-        default: () => [{
-        selectColumnTabModel: {
+const stopResize = () => {
+  document.removeEventListener('mousemove', doResize);
+  document.removeEventListener('touchmove', doResize);
+  document.removeEventListener('mouseup', stopResize);
+  document.removeEventListener('touchend', stopResize);
+};
+
+const emit = defineEmits<Emits>()
+const  form=ref({});
+const  tableFormRules=ref({
+    tableEnName: [
+      { required: true, message: "请选择表", trigger: "blur" }
+    ],
+  })
+const  treeModelDef=ref([])
+const  openSelectTab=ref(false)
+const designModel =ref({
+  selectColumnTabModel:{},//列表模型
+  distinct:Boolean,//是否排重
+  conditionTreeModel:{},//条件列树模型
+  groupConditionTreeModel:{},//分组列树模型
+  tablesModel:[],//表模型列表
+  sortColumnModel:[],//排序列模型
+  tableJoinModels:[],//表关系模型
+})
+const activeName=ref('columnTab')
+
+const props= defineProps({
+  designModel: {
+    type:Object,
+    default: () => [{
+    selectColumnTabModel: {
+      type: Array,
+      default: () => [{
+        columAndExp: String,//列/表达式
+        alias: String,//alias
+        aggregation: String,//聚合
+        group: Boolean,//是否分组
+        chineseName: String,//中文名
+        orgTableName: String,//原始表名
+        tableAlias: String,//表别名
+        fieldName: String,//字段名
+        datasourceName:String,//数据源名称
+      }]
+    },
+    distinct: {
+      type: Boolean
+    },
+    conditionTreeModel: {
+      type: Object,
+      default: () => [{
+        type: Number,
+        conditionRelaType:String,//条件关系类型:All,Any,None,NotAll
+        parentLevel: String,//父级层次
+        currentLevel: Number,//当前级别
+        childConditionTreeModels: Array,//子树模型
+        left: String,//	左边操作列/表达式
+        operator: String,//操作符
+        right: String,// 右边操作列/表达式
+        id:String,//行唯一标识=parentLevel+"."+currentLevel
+      }]
+    },
+    groupConditionTreeModel: {
+      type: Object
+    },
+    tablesModel: {
+      type: Array,
+      default: () => [{
+        orgTableName: String,//原始表名
+        tableAlias: String,//表别名
+        tableCnName: String,//表中文名
+        datasourceName: String,//数据源
+        x: Number,
+        y: Number,
+        h: Number,
+        w: Number,
+        active: Boolean,
+        columns: {
           type: Array,
           default: () => [{
-            columAndExp: String,//列/表达式
-            alias: String,//alias
-            aggregation: String,//聚合
-            group: Boolean,//是否分组
-            chineseName: String,//中文名
-            orgTableName: String,//原始表名
-            tableAlias: String,//表别名
-            fieldName: String,//字段名
-            datasourceName:String,//数据源名称
+            fieldName: String,//字段英文名
+            fieldCnName: String,//字段中文名
           }]
-        },
-        distinct: {
-          type: Boolean
-        },
-        conditionTreeModel: {
-          type: Object,
-          default: () => [{
-            type: Number,
-            conditionRelaType:String,//条件关系类型:All,Any,None,NotAll
-            parentLevel: String,//父级层次
-            currentLevel: Number,//当前级别
-            childConditionTreeModels: Array,//子树模型
-            left: String,//	左边操作列/表达式
-            operator: String,//操作符
-            right: String,// 右边操作列/表达式
-            id:String,//行唯一标识=parentLevel+"."+currentLevel
-          }]
-        },
-        groupConditionTreeModel: {
-          type: Object
-        },
-        tablesModel: {
+        }
+      }]
+    },
+    sortColumnModel: {
+      type: Array,
+      default: () => [{
+        orgTableName: String,//原始表名
+        tableAlias: String,//表别名
+        columAndExp: String,//列/表达式
+        alias: String,//列别名
+        fieldName: String,//字段名
+        descending: Boolean,//是否倒排
+      }]
+    },
+    tableJoinModels: {
+      type: Array,
+      default: () => [{
+        joinType: String,//表关联类型
+        tableAlias: String,//表别名
+        tableCnName: String,//表中文名
+        dbColumnJoinModels: {
           type: Array,
           default: () => [{
-            orgTableName: String,//原始表名
-            tableAlias: String,//表别名
-            tableCnName: String,//表中文名
-            datasourceName: String,//数据源
-            x: Number,
-            y: Number,
-            h: Number,
-            w: Number,
-            active: Boolean,
-            columns: {
-              type: Array,
+            operatorSign: String,//操作符
+            startColumnName: String,//开始列名
+            endColumnName: String,//结束列名
+            startPt: {
+              type: Object,
               default: () => [{
-                fieldName: String,//字段英文名
-                fieldCnName: String,//字段中文名
+                x: Number,
+                y: Number
+              }]
+            },//开始点
+            endPt: {
+              type: Object,
+              default: () => [{
+                x: Number,
+                y: Number
+              }]
+            },//结束点
+            startIndex: Number,//开始索引
+            endIndex: Number,//结束索引
+            modelRect: {
+              type: Object,
+              default: () => [{
+                x: Number,
+                y: Number,
+                width: Number,
+                height: Number
               }]
             }
           }]
-        },
-        sortColumnModel: {
-          type: Array,
-          default: () => [{
-            orgTableName: String,//原始表名
-            tableAlias: String,//表别名
-            columAndExp: String,//列/表达式
-            alias: String,//列别名
-            fieldName: String,//字段名
-            descending: Boolean,//是否倒排
-          }]
-        },
-        tableJoinModels: {
-          type: Array,
-          default: () => [{
-            joinType: String,//表关联类型
-            tableAlias: String,//表别名
-            tableCnName: String,//表中文名
-            dbColumnJoinModels: {
-              type: Array,
-              default: () => [{
-                operatorSign: String,//操作符
-                startColumnName: String,//开始列名
-                endColumnName: String,//结束列名
-                startPt: {
-                  type: Object,
-                  default: () => [{
-                    x: Number,
-                    y: Number
-                  }]
-                },//开始点
-                endPt: {
-                  type: Object,
-                  default: () => [{
-                    x: Number,
-                    y: Number
-                  }]
-                },//结束点
-                startIndex: Number,//开始索引
-                endIndex: Number,//结束索引
-                modelRect: {
-                  type: Object,
-                  default: () => [{
-                    x: Number,
-                    y: Number,
-                    width: Number,
-                    height: Number
-                  }]
-                }
-              }]
-            }
-          }]
-        },
-        }]
         }
-    })
-
-    const vResize:Directive = {
-        bind(el, binding) {
-          const onResize = () => {
-            let height = window.innerHeight - el.getBoundingClientRect().top;
-            el.style.height = `${height}px`;
-            binding.value && binding.value();
-          };
-          onResize();
-          window.addEventListener('resize', onResize);
-
-          el['onResize'] = onResize;
-        },
-        unbind(el) {
-          window.removeEventListener('resize', el['onResize']);
-        },
-    };
-    function  onResize() {
-        // 这里可以添加你想要执行的代码，比如更新数据等
-      }
-    function  handleAdd() {
-        openSelectTab.value = true
-      }
-    function  handleDelete(){
-      designModel.value.tablesModel = [];
-     }
-    async function selectTable() {
-      //检查目前是否有模型，有则提示，只能增加一个表;否则模型列表增加一个
-      if (designModel.value.tablesModel.length > 0) {
-        ElMessage('目前只支持一张表!请删除后再加入');
-        return
-      } else {
-        //根据模型定义id查询模型
-        let resp = await getDef(form.modelDefId)
-        let modelDef = resp.data;
-        //构造新模型加入模型列表
-        let newModelDef={
-          alias:'',
-          cnName:'',
-          enName:'',
-          x: 100,
-          y: 100,
-          h: 100,
-          w: 100,
-          active: false,
-          datasourceName:'',
-          columns:[]
-        }
-        resp = await getAllField(modelDef);
-        newModelDef.cnName = modelDef.cnName;
-        newModelDef.enName = modelDef.enName;
-        newModelDef.datasourceName = modelDef.datasourceName;
-        for(let i=0;i<resp.data.length;i++){
-          let item = resp.data[i];
-          let tmpItem = {fieldName:item.fieldName,fieldCnName:item.fieldCnName}
-          newModelDef.columns.push(tmpItem)
-        }
-        designModel.value.tablesModel.push(newModelDef);
-      }
-      openSelectTab.value = false;
-
+      }]
+    },
+    }]
     }
-      /** 获取接口树列表 */
-      function getTreeModelDef(){
-      let tmp={};
-      tree(tmp).then(response => {
-        treeModelDef.value = response.data;
-      });
-     }
-     function updateTableDefine( tableDefineItems) {
-       designModel.value.tablesModel = JSON.parse(JSON.stringify(tableDefineItems));
-       //更新父窗口模型
-       emit('updateDesignModel', designModel.value);
-     }
-     function updateDistinct(value){
-       designModel.value.distinct = value;
-     }
-     onMounted(()=>{
-       getTreeModelDef()
-    })
-    watch(() => props, val => {
-      debugger;
-      designModel.value.selectColumnTabModel = JSON.parse(JSON.stringify(props.designModel.selectColumnTabModel));
-      designModel.value.distinct = props.designModel.distinct;
-      designModel.value.conditionTreeModel = JSON.parse(JSON.stringify(props.designModel.conditionTreeModel));
-      designModel.value.groupConditionTreeModel = JSON.parse(JSON.stringify(props.designModel.groupConditionTreeModel));
-      designModel.value.tablesModel = JSON.parse(JSON.stringify(props.designModel.tablesModel));
-      designModel.value.sortColumnModel = JSON.parse(JSON.stringify(props.designModel.sortColumnModel));
-      designModel.value.tableJoinModels = JSON.parse(JSON.stringify(props.designModel.tableJoinModels));
-    });
-    //页签单击
-    function handleElTabClick(tab, event){
+})
 
+
+function  handleAdd() {
+    openSelectTab.value = true
+  }
+function  handleDelete(){
+  designModel.value.tablesModel = [];
+ }
+async function selectTable() {
+  //检查目前是否有模型，有则提示，只能增加一个表;否则模型列表增加一个
+  if (designModel.value.tablesModel.length > 0) {
+    ElMessage('目前只支持一张表!请删除后再加入');
+    return
+  } else {
+    //根据模型定义id查询模型
+    let resp = await getDef(form.modelDefId)
+    let modelDef = resp.data;
+    //构造新模型加入模型列表
+    let newModelDef={
+      alias:'',
+      cnName:'',
+      enName:'',
+      x: 100,
+      y: 100,
+      h: 100,
+      w: 100,
+      active: false,
+      datasourceName:'',
+      columns:[]
     }
+    resp = await getAllField(modelDef);
+    newModelDef.cnName = modelDef.cnName;
+    newModelDef.enName = modelDef.enName;
+    newModelDef.datasourceName = modelDef.datasourceName;
+    for(let i=0;i<resp.data.length;i++){
+      let item = resp.data[i];
+      let tmpItem = {fieldName:item.fieldName,fieldCnName:item.fieldCnName}
+      newModelDef.columns.push(tmpItem)
+    }
+    designModel.value.tablesModel.push(newModelDef);
+  }
+  openSelectTab.value = false;
+
+}
+  /** 获取接口树列表 */
+  function getTreeModelDef(){
+  let tmp={};
+  tree(tmp).then(response => {
+    treeModelDef.value = response.data;
+  });
+ }
+ function updateTableDefine( tableDefineItems) {
+   designModel.value.tablesModel = JSON.parse(JSON.stringify(tableDefineItems));
+   //更新父窗口模型
+   emit('updateDesignModel', designModel.value);
+ }
+ function updateDistinct(value){
+   designModel.value.distinct = value;
+ }
+ onMounted(()=>{
+   getTreeModelDef()
+})
+watch(() => props, val => {
+  debugger;
+  designModel.value.selectColumnTabModel = JSON.parse(JSON.stringify(props.designModel.selectColumnTabModel));
+  designModel.value.distinct = props.designModel.distinct;
+  designModel.value.conditionTreeModel = JSON.parse(JSON.stringify(props.designModel.conditionTreeModel));
+  designModel.value.groupConditionTreeModel = JSON.parse(JSON.stringify(props.designModel.groupConditionTreeModel));
+  designModel.value.tablesModel = JSON.parse(JSON.stringify(props.designModel.tablesModel));
+  designModel.value.sortColumnModel = JSON.parse(JSON.stringify(props.designModel.sortColumnModel));
+  designModel.value.tableJoinModels = JSON.parse(JSON.stringify(props.designModel.tableJoinModels));
+});
+//页签单击
+function handleElTabClick(tab, event){
+
+}
 
 /**
  * 判断表列是否在排序列模型
@@ -343,17 +352,20 @@ function makeSortColumn(tablesModelElement: any, column: any) {
 </script>
 
 <style>
-.resizable {
-  overflow: auto;
-  resize: vertical;
-  cursor: n-resize;
-  /* 设置一个初始高度 */
-  height: calc(100vh - 100px);
-  /* 设置边框以便于拖拽 */
-  border-top: 2px solid #000;
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
-
-.resizable:hover {
-  border-top-color: #333;
+.top, .bottom {
+  resize: vertical;
+  overflow: auto;
+}
+.splitter {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  height: 10px;
+  cursor: ns-resize;
+  background-color: #ccc;
 }
 </style>
