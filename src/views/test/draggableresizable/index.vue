a7666aef0932712cb17da1962d942ee6fc8fa336
longsebo <7432731@qq.com>
2024-05-13 12:51:13 +0800

    增加测试拖动块改变上下div高度

--- src/views/test/draggableresizable/index.vue
+++ src/views/test/draggableresizable/index.vue
@@ -1,95 +1,63 @@
 <template>
-  <div id="app">
-    <div class="parent">
-      <Vue3DraggableResizable
-          :initW="110"
-          :initH="120"
-          v-model:x="x"
-          v-model:y="y"
-          v-model:w="w"
-          v-model:h="h"
-          v-model:active="active"
-          :draggable="true"
-          :resizable="true"
-          @activated="print('activated')"
-          @deactivated="print('deactivated')"
-          @drag-start="print('drag-start')"
-          @resize-start="print('resize-start')"
-          @dragging="print('dragging')"
-          @resizing="print('resizing')"
-          @drag-end="print('drag-end')"
-          @resize-end="print('resize-end')"
-      >
-        <el-row>
-          <el-col>
-            <el-text class="mx-1" type="primary">人员信息</el-text>
-          </el-col>
-          <el-col>
-            <el-input v-model="input" style="width: 100%" placeholder="请输入别名" />
-          </el-col>
-          <el-col>
-            <el-table
-                ref="multipleTableRef"
-                :data="tableData"
-                style="width: 100%"
-                border
-            >
-              <el-table-column type="selection" width="55" />
-              <el-table-column label="字段名" width="120">
-                <template #default="scope">{{ scope.row.fieldName }}</template>
-              </el-table-column>
-
-            </el-table>
-          </el-col>
-        </el-row>
-      </Vue3DraggableResizable>
+  <div class="container">
+    <div class="top" :style="{ height: topHeight + 'px' }">
+      顶部内容
+    </div>
+    <div class="splitter" @mousedown="startResize" @touchstart="startResize"></div>
+    <div class="bottom" :style="{ height: bottomHeight + 'px' }">
+      底部内容
     </div>
   </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-import Vue3DraggableResizable from 'vue3-draggable-resizable'
-//default styles
-import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
-export default defineComponent({
-  components: { Vue3DraggableResizable },
-  data() {
-    return {
-      x: 100,
-      y: 100,
-      h: 100,
-      w: 100,
-      active: false,
-      input:'',
-      tableData: [
-      {
-        fieldName: 'name',
-      },
-      {
-        fieldName: 'age',
-      },
-      {
-        fieldName: 'sex',
-      }
-    ]
-    }
-  },
-  methods: {
-    print(val) {
-      console.log(val)
-    }
-  }
-})
+<script setup>
+import { ref } from 'vue';
+
+const topHeight = ref(100);
+const bottomHeight = ref(300);
+let startY = 0;
+let startTopHeight = 0;
+
+const startResize = (event) => {
+  event.preventDefault();
+  startY = event.clientY;
+  startTopHeight = topHeight.value;
+  document.addEventListener('mousemove', doResize);
+  document.addEventListener('touchmove', doResize);
+  document.addEventListener('mouseup', stopResize);
+  document.addEventListener('touchend', stopResize);
+};
+
+const doResize = (event) => {
+  event.preventDefault();
+  const diff = event.clientY - startY;
+  topHeight.value = startTopHeight + diff;
+  bottomHeight.value = 500 - topHeight.value; // 假设容器高度是500px
+};
+
+const stopResize = () => {
+  document.removeEventListener('mousemove', doResize);
+  document.removeEventListener('touchmove', doResize);
+  document.removeEventListener('mouseup', stopResize);
+  document.removeEventListener('touchend', stopResize);
+};
 </script>
+
 <style>
-.parent {
-  width: 200px;
-  height: 200px;
-  position: absolute;
-  top: 100px;
-  left: 100px;
-  border: 1px solid #000;
-  user-select: none;
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+}
+.top, .bottom {
+  resize: vertical;
+  overflow: auto;
+}
+.splitter {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  height: 10px;
+  cursor: ns-resize;
+  background-color: #ccc;
 }
 </style>
