ce91d36d56864041304480e9c54db9eb12bd7af9
longsebo <lxb741126>
2024-06-11 21:39:35 +0800

    查询条件增加可选项

--- src/views/business/designsql/searchConditionTabPane.vue
+++ src/views/business/designsql/searchConditionTabPane.vue
@@ -31,7 +31,7 @@
           <el-col :span="3">
             <el-select
                 v-model="scope.row.operator"
-                style="width: 70px"
+                style="width: 75px"
             >
               <el-option
                   v-for="item in operators"
@@ -44,8 +44,10 @@
           <el-col :span="6">
             <el-input v-model="scope.row.right" placeholder="请输入表达式或点击右边...按钮"/>
           </el-col>
-          <el-col :span="5">
+          <el-col :span="2">
             <el-button size="small" @click="showSelectFieldOrParamDlg(scope.row,'right')">...</el-button>
+          </el-col>
+          <el-col :span="3">
             <el-checkbox true-value="1" false-value="0" v-model="scope.row.isOptional" label="是否可选"></el-checkbox>
           </el-col>
         </el-row>
